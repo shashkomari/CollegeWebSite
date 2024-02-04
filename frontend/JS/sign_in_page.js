d3e6af2f367a1ad2786272be1ae0c7a2bd61ae5d
MuzicenkoAnastasia <muzicenkrnasta@gmail.com>
2024-02-04 16:15:11 +0100

    Design for sign_in_page and sending JSON file

---
+++ frontend/JS/sign_in_page.js
@@ -0,0 +1,37 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+
+    event.preventDefault(); // Щоб сторінка не перезавантажувалася при натисканні кнопки
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+   
+    // Будуємо об'єкт JSON
+           var data = {
+             email: parseInt(email),
+             password: parseInt(password)
+           };
+  
+    
+           // Відправляємо POST-запит
+           fetch('http://localhost:8080/sign_in', {
+             method: 'POST',
+             headers: {
+               'Content-Type': 'application/json'
+             },
+             body: JSON.stringify(data)
+           })
+           .then(function(response) {
+             if (response.ok) {
+               // Обробляємо відповідь
+               console.log(response);
+             } else {
+               response.json().then(function(data) {
+               var errorMessage = data.Error;
+               alert(errorMessage);
+             });
+             }
+           })
+           .catch(function(error) {
+             // Обробляємо помилку
+             console.error(error);
+           });
+    });
