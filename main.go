df5a2b1b971a56fd92e2bbda0c8baa8df601ad34
Mariana Shashko <shashkomari@gmail.com>
2024-02-05 13:41:11 +0200

    HTML, CSS and JS files are opened in response to a request to the HTTP server

--- main.go
+++ main.go
@@ -1,12 +1,70 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
+	"path/filepath"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./frontend/HTML/main_page.html")
+	r := gin.Default()
+
+	// Задаємо абсолютний шлях до каталогу templates
+	templatesPath, _ := filepath.Abs("frontend/HTML")
+	r.LoadHTMLGlob(templatesPath + "/*")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(200, "main_page.html", gin.H{})
 	})
-	http.ListenAndServe(":8080", nil)
+
+	// Serve HTML page
+	r.GET("/sing_in/", func(c *gin.Context) {
+		c.HTML(200, "sign_in_page.html", gin.H{})
+	})
+
+	// Serve static files (CSS, JS)
+	r.Static("/static", "./frontend")
+
+	port := 8080
+	fmt.Printf("Server is running on http://localhost:%d\n", port)
+	r.Run(fmt.Sprintf(":%d", port))
 }
+
+// package main
+
+// import (
+// 	"database/sql"
+// 	"net/http"
+
+// 	"github.com/gin-gonic/gin"
+
+// 	"projects/CollegeWebSite/backend/handlers"
+// 	"projects/CollegeWebSite/backend/repositories"
+// 	"projects/CollegeWebSite/backend/services"
+
+// 	_ "github.com/go-sql-driver/mysql"
+// )
+
+// func main() {
+// 	r := gin.Default()
+// 	r.LoadHTMLGlob("HTML/*")
+// 	r.Static("/css", "./frontend/CSS/")
+
+// 	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/CourseWork")
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
+
+// 	lectureRepository := repositories.NewLectureRepository(db)
+// 	lectureServices := services.NewLectureService(lectureRepository)
+// 	lectureHandlers := handlers.NewLectureHttp(lectureServices)
+
+// 	r.GET("/", lectureHandlers.GetLectures)
+
+// 	r.GET("/", func(c *gin.Context) {
+// 		c.HTML(http.StatusOK, "main_page.html", gin.H{})
+// 	})
+
+// 	r.Run()
+// }
